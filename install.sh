19193818be37b72036413bb6735819a4a375159e
Oleksii Novikov <hello@theoleksii.me>
2025-09-24 21:04:32 +0000

    feat: configures nvm

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ if ! is_ubuntu && ! is_mac; then
   exit 1
 fi
 
-modules=("big-bang" "zsh" "ghostty" "tmux")
+modules=("big-bang" "zsh" "ghostty" "tmux" "node")
 
 for module_name in "${modules[@]}"; do
   MODULE_NAME="$module_name"
---
+++ node/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Bash doesn't know about NVM
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+if ! has_command nvm; then
+  if [[ ! -d "$NVM_DIR" ]]; then
+    git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  else 
+    git -C "$NVM_DIR" fetch --tags --quiet
+  fi
+  
+  latest_tag="$(git -C "$NVM_DIR" describe --abbrev=0 --tags --match 'v[0-9]*' "$(git -C "$NVM_DIR" rev-list --tags --max-count=1)")"
+  git -C "$NVM_DIR" -c advice.detachedHead=false checkout "$latest_tag"
+
+  \. "$NVM_DIR/nvm.sh"
+
+  nvm install --lts
+fi
