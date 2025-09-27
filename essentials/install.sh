bc8746a4936d7a98032a67af9f2408b10df3b54d
Oleksii Novikov <hello@theoleksii.me>
2025-09-27 09:59:01 +0000

    feat: installs gum

--- essentials/install.sh
+++ essentials/install.sh
@@ -33,4 +33,31 @@ if is_ubuntu; then
   sudo apt-get upgrade
 fi
 
-source "$MODULE_DIR/gcc/install.sh"
+if ! has_command gcc; then
+  if is_mac; then
+    if xcode-select --install 1>/dev/null; then
+      info "Xcode Command Line Tools installation started"
+      info "After installation, re-run this script"
+      exit -1
+    else
+      err "Failed to start Xcode Command Line Tools installation"
+      exit 0
+    fi
+
+  elif is_ubuntu; then
+    info "Installing build-essential..."
+    sudo apt-get install -y build-essential libncursesw4-dev
+  fi
+fi
+
+if ! has_command gum; then
+  if is_mac; then
+    brew install gum
+
+  elif is_ubuntu; then
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+    sudo apt update && sudo apt install gum
+  fi
+fi
