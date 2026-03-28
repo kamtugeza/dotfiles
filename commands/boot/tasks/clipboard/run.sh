792474a7404a5e043fbae4a1d0a7703117e6033c
Oleksii Novikov <hello@theoleksii.me>
2026-03-28 22:07:28 +0200

    feat(arch): configure keyboard

--- commands/boot/tasks/clipboard/run.sh
+++ commands/boot/tasks/clipboard/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 link "${DOTFILES_HOME}/configs/config/hypr/modules/autostart.conf" "${XDG_CONFIG_HOME}/hypr/modules/autostart.conf"
+link "${DOTFILES_HOME}/configs/config/hypr/modules/input.conf" "${XDG_CONFIG_HOME}/hypr/modules/input.conf"
 link "${DOTFILES_HOME}/configs/config/hypr/modules/keybindings.conf" "${XDG_CONFIG_HOME}/hypr/modules/keybindings.conf"
 
